ea0e894cd467676867f4bb39a18c45351ac793da
cruzrovira <cruzrovira@gmail.com>
2022-06-19 13:01:20 -0600

    change main soport css module storybook

--- components/.storybook/main.js
+++ components/.storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-css-modules",
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
 }
